31f78e552ff6b9c6904bdea00ae949dc3cd26947
telesdte <ltelesduarte@gmail.com>
2025-09-06 15:43:45 -0300

    Configurando a classe conexão

--- ProjetoVeterinario/ProjetoVeterinario/Conexao.cs
+++ ProjetoVeterinario/ProjetoVeterinario/Conexao.cs
@@ -10,5 +10,33 @@ namespace ProjetoVeterinario
     internal class Conexao
     {
         MySqlConnection Con = new MySqlConnection("Data Source=localhost;Initial Catalog=BDPizzaria1;user=root;pwd=123456");
+
+        public static string msg;
+
+        public MySqlConnection ConnectarBD()
+        {
+            try
+            {
+                Con.Open();
+            }
+            catch (Exception erro)
+            {
+                msg = "ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return Con;
+        }
+
+        public MySqlConnection DesconnectarBD()
+        {
+            try
+            {
+                Con.Close();
+            }
+            catch(Exception erro)
+            {
+                msg = "ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return Con;
+        }
     }
 }
